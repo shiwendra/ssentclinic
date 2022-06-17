614af37af8b4ad6d4ffc61a8b0ed86d07528acc3
Shiwendra <shiwendra@gmail.com>
2022-06-17 10:28:50 +0530

    formatted the code

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,34 +1,25 @@
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 import crData from "./CrouselData";
-import {useState} from "react";
+import { useState } from "react";
 
 function CarouselMome() {
-  const [items, setItem]=useState(crData);
+  const [items, setItem] = useState(crData);
   return (
     <Carousel>
-      
-      {
-       items.map((data, index) => {
-     
+      {items.map((data, index) => {
         return (
           <Carousel.Item interval={1000}>
-          <img
-            className="d-block w-100"
-            src={data.image}
-            alt={data.name}
-            height={550}
-          />
- 
- </Carousel.Item>
-                
+            <img
+              className="d-block w-100"
+              src={data.image}
+              alt={data.name}
+              height={550}
+            />
+          </Carousel.Item>
         );
-      }
-      
-      )
-    }
-   
+      })}
     </Carousel>
   );
 }
 
-export default CarouselMome
+export default CarouselMome;
